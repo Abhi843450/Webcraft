1793a3e69dc22f6b989cb984df77055adbac0eda
Abhi843450 <prazapatiavi@gmail.com>
2026-09-13 16:22:12 +0545

    debug content type

---
+++ api/debug_ct.php
@@ -0,0 +1,8 @@
+﻿<?php
+header('Content-Type: application/json');
+echo json_encode([
+    'CONTENT_TYPE' => $_SERVER['CONTENT_TYPE'] ?? null,
+    'HTTP_CONTENT_TYPE' => $_SERVER['HTTP_CONTENT_TYPE'] ?? null,
+    'REQUEST_CONTENT_TYPE' => $_SERVER['HTTP_CONTENT_TYPE'] ?? $_SERVER['CONTENT_TYPE'] ?? null,
+    'ALL_SERVER' => array_filter($_SERVER, function($k) { return stripos($k, 'content') !== false; }, ARRAY_FILTER_USE_KEY)
+]);
